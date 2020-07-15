59f8cb9740d06c6ad2e770659464c2d5da13ada2
miv42 <62116681+miv42@users.noreply.github.com>
2020-07-15 12:44:51 +0300

    delet all

--- resources/views/admin/includes/layout.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        @include('admin/includes.header')
-        @include('admin/includes.sidebar')
-        @yield('content')
-        @include('admin/includes.footer')
-    </body>
-</html>
