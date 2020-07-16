da7ad2f9f7f4e4adf555362d03959da1e0198fda
dudulics112 <50314275+dudulics112@users.noreply.github.com>
2020-07-16 10:41:36 +0300

    hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,6 +63,7 @@
         </style>
     </head>
     <body>
+        <a> Hello world!</a>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
